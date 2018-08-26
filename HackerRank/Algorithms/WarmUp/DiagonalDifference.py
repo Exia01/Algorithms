8c8595cbf89e630aee0f48e9293127c83bc70ed2
Jose <sixgg4@gmail.com>
2018-08-25 21:25:37 -0400

    Finished Diagonal Differences.

---
+++ HackerRank/Algorithms/WarmUp/DiagonalDifference.py
@@ -0,0 +1,19 @@
+from math import fabs
+arr = [
+    [11, 2, 4],
+    [4, 5, 6],
+    [10, 8, - 12],
+    ]
+n = 3
+def diagonalDifference(arr, n):
+    sum1 = 0
+    sum2 = 0
+    # sum1 = sum(arr[i][i] for i in range(n))
+    # sum2 = sum(arr[i][n - i - 1] for i in range(n))
+    # print(sum1)
+    # print(sum2)
+    # return str(int(sum2 - sum1))
+    difference = sum(row[i] - row[-i - 1] for i, row in enumerate(arr))
+    return abs(difference)
+ 
+print(diagonalDifference(arr,n))
